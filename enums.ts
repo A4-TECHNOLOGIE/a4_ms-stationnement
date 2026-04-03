dd10c138efa9a30ca184ea1d705a1db80802df6f
A4-TECHNOLOGIE <technicien.sav@a4.fr>
2026-04-03 16:01:50 +0200

    Create enums.ts

---
+++ enums.ts
@@ -0,0 +1,39 @@
+//% color=#FF1493
+enum RGBIndex {
+    //% block="RGB0"
+    RGB0 = 0,
+    //% block="RGB1"
+    RGB1 = 1,
+    //% block="les deux"
+    Both = 2
+}
+
+//% color=#FF1493
+enum RGBColor {
+    //% block="rouge"
+    Red,
+    //% block="vert"
+    Green,
+    //% block="bleu"
+    Blue,
+    //% block="jaune"
+    Yellow,
+    //% block="cyan"
+    Cyan,
+    //% block="magenta"
+    Magenta,
+    //% block="blanc"
+    White,
+    //% block="éteint"
+    Off
+}
+
+//% color=#FF8C00
+enum IO {
+    C0 = 0,
+    C1,
+    C2,
+    C3,
+    C4,
+    C5
+}
